ddc66d4488163499dbe6eba53118ce9ca71c5102
VRN4K <VRN4K@users.noreply.github.com>
2025-04-29 12:29:16 +0000

    deploy: 1829b48c2980856bd4afd740d42daa8a72b596f6

--- src/helpers/errorMessages.js
+++
@@ -1,4 +0,0 @@
-export default {
-    BUG_ERROR_MESSAGE: "What did you find out?",
-    CRASH_ERROR_MESSAGE: "You found a crash bug, examine the page"
-};
--- src/pages/app.js
+++
@@ -1,9 +0,0 @@
-import {MainPage} from "./main.page";
-import {ProductPage} from "./product.page";
-
-export class App {
-    constructor(page) {
-        this.main = new MainPage(page);
-        this.product = new ProductPage(page);
-    }
-}
